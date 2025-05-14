31f7eaaeb013daf207144403e13047ea573a7807
Ari <acas_2014@outlook.es>
2025-05-14 10:55:59 -0500

    feat: create purchase-orders view

--- src/features/shared/models/purchase-order.entity.js
+++ src/features/shared/models/purchase-order.entity.js
@@ -3,19 +3,21 @@ export class PurchaseOrderEntity {
         id = 0,
         inventory_part_id = 0,
         quantity = 0,
-        price = 0.00,
+        price = 0,
         order_date = '',
         received_date = null,
         status = 'PENDING',
-        user_id = 0
+        user_id = 0,
+        inventoryPart = null
     }) {
         this.id = id;
-        this.inventoryPartId = inventory_part_id;
+        this.inventory_part_id = inventory_part_id;
         this.quantity = quantity;
         this.price = price;
-        this.orderDate = order_date;
-        this.receivedDate = received_date;
+        this.order_date = order_date;
+        this.received_date = received_date;
         this.status = status;
-        this.userId = user_id;
+        this.user_id = user_id;
+        this.inventoryPart = inventoryPart;
     }
 }
--- src/router/index.js
+++ src/router/index.js
@@ -2,12 +2,14 @@ import { createRouter, createWebHistory } from "vue-router";
 import ComponentsDemo from '../shared/views/components-demo.vue'
 import MaintenancePlanView from '../features/maintenance-plan/views/maintenance-plan-view.component.vue'
 import InventoryPartsView from '../features/inventory-parts/views/inventory-parts.view.component.vue'
+import PurchaseOrdersView from '../features/purchase-orders/views/purchase-orders.view.component.vue'
 const router = createRouter({
     history: createWebHistory(),
     routes: [
         { path: '/', component: ComponentsDemo, name: 'components-demo' },
         { path: '/maintenance-plan', component: MaintenancePlanView, name: 'maintenance-plan' },
         { path: '/inventory-parts', component: InventoryPartsView, name: 'inventory-parts' },
+        { path: '/purchase-orders', component: PurchaseOrdersView, name: 'purchase-orders' },
  /*        { path: '/chi', component: Chi, name: 'chi' },
         { path: '/nio', component: Nio, name: 'nio' }, */
     ]
