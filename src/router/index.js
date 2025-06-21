566b15ac2976a70142c430ac6dde74c7bf59ef8b
Claus <cbm4y0@gmail.com>
2025-06-20 23:43:08 -0500

    j

--- src/router/index.js
+++ src/router/index.js
@@ -4,12 +4,15 @@ import MaintenancePlanView from '../features/maintenance-plan/views/maintenance-
 import InventoryPartsView from '../features/inventory-parts/views/inventory-parts.view.component.vue'
 import CalendarViewComponent from "../features/maintenance-calendar/views/calendar.view.component.vue";
 import PurchaseOrdersView from '../features/purchase-orders/views/purchase-orders.view.component.vue'
+
 import NotFoundView from '../shared/views/not-found.view.component.vue'
 import MachineryAssetView from '../features/asset-management/views/machinery-asset-view.component.vue'
 import LoginView from '../features/authentication/views/login.view.component.vue'
 import CreateAccountView from '../features/authentication/views/create-account.view.component.vue'
 import HomeView from '../public/pages/home-view.vue'
 import ExecutionView from '../features/execution/views/execution-view.component.vue'
+import WorkOrderView from '../features/work-order/views/work-order.component.vue'
+
 
 const router = createRouter({
     history: createWebHistory(),
@@ -21,6 +24,7 @@ const router = createRouter({
         { path: '/machinery', component: MachineryAssetView, name: 'machinery' },
         { path: '/login', component: LoginView, name: 'login' },
         {path: '/create-account',component:CreateAccountView,name: 'create-account'},
+        {path: '/orden-trabajo',component:WorkOrderView,name: 'orden-trabajo'},
         { path: '/404', component: NotFoundView, name: 'not-found' }, 
         { path: '/:pathMatch(.*)*', redirect: '/404' },
         { path: '/', component: HomeView, name: 'home' },
